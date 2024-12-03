5f9dc730616aed7c8a19dba8956a3752185115fb
raed-abodayy <raedabodyu@gmail.com>
2024-12-03 15:16:15 +0200

    testing if git is working

--- Assets/scripts/RaceState.cs
+++ Assets/scripts/RaceState.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public enum RaceState
 {
-Start,
-Accelerate,
-Turn,
-Crash,
-Finish,
+    Start,
+    Accelerate,
+    Turn,
+    Crash,
+    Finish,
 }
 
--- Assets/scripts/RacingGame.cs
+++ Assets/scripts/RacingGame.cs
@@ -7,36 +7,36 @@ public class RacingGame : MonoBehaviour
     public RaceState raceState;
 
     public void SimulateRace()
-{
-switch (raceState)
-{
-    case RaceState.Start:
-    Debug.Log("The race is about to begin. Get ready!");
-    break;
+    {
+        switch (raceState)
+        {
+            case RaceState.Start:
+                Debug.Log("The race is about to begin. Get ready!");
+                break;
 
-    case RaceState.Accelerate:
-    Debug.Log("You pressed the gas pedal.The car speeds up!");
-    break;
+            case RaceState.Accelerate:
+                Debug.Log("You pressed the gas pedal.The car speeds up!");
+                break;
 
-    case RaceState.Turn:
-    Debug.Log("You approach a sharp turn. be careful not to crash!");
-    break;
+            case RaceState.Turn:
+                Debug.Log("You approach a sharp turn. be careful not to crash!");
+                break;
 
-    case RaceState.Crash:
-    Debug.Log("You it a barrier.The race is over.");
-    break;
+            case RaceState.Crash:
+                Debug.Log("You it a barrier.The race is over.");
+                break;
 
-    case RaceState.Finish:
-    Debug.Log("You crossed the finish line.Well done!");
-    break;
+            case RaceState.Finish:
+                Debug.Log("You crossed the finish line.Well done!");
+                break;
 
-    default:
-    Debug.LogWarning("This state is not defined.Something is wrong.");
-    break;
-}
-}
+            default:
+                Debug.LogWarning("This state is not defined.Something is wrong.");
+                break;
+        }
+    }
     void Update()
     {
-SimulateRace();
+        SimulateRace();
     }
 }
