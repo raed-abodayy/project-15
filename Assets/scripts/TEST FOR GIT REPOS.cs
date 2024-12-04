8231395baad7cd27663926ec37eae36f6e012896
raed-abodayy <raedabodyu@gmail.com>
2024-12-04 16:08:04 +0200

    only a TEST

---
+++ Assets/scripts/TEST FOR GIT REPOS.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TESTFORGITREPOS : MonoBehaviour
+{
+
+    void Start()
+    {
+     Debug.Log("test for git repos");
+    }
+
+}
